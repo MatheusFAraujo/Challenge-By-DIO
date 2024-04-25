77d1151fd8208baad697cb5dc8e2b4e9dd2628e2
Matheus Araujo <167690518+MatheusFAraujo@users.noreply.github.com>
2024-04-25 20:06:23 +0530

    desafio do heroi atualizadot

--- ClassificadorHeroi.js
+++ ClassificadorHeroi.js
@@ -6,35 +6,30 @@ const heroi = {
   }
   
   // Verificando o nível do herói e atribuindo sua liga
-      if (heroi.nivel < 1000) {
-    heroi.liga = "Ferro"
+
+       if (heroi.nivel <= 1000) {
+      heroi.liga = "Ferro"
   } 
-  
-      else if (heroi.nivel > 1000 && heroi.nivel < 2000) {
-    heroi.liga = "Bronze"
+       else if (heroi.nivel >= 1000 && heroi.nivel <= 2000) {
+         heroi.liga = "Bronze"
     } 
-  
-      else if (heroi.nivel > 2000 && heroi.nivel < 5000){
-      heroi.liga = "Prata"
+       else if (heroi.nivel >= 2000 && heroi.nivel <= 5000){
+         heroi.liga = "Prata"
       }
-  
-      else if (heroi.nivel > 5001 && heroi.nivel < 7000){
-        heroi.liga = "Ouro"
+       else if (heroi.nivel >= 5001 && heroi.nivel <= 7000){
+         heroi.liga = "Ouro"
       }
-  
-       else if (heroi.nivel > 7001 && heroi.nivel < 8000) {
-        heroi.liga = "Platina"
+       else if (heroi.nivel >= 7001 && heroi.nivel <= 8000) {
+         heroi.liga = "Platina"
       }
-  
-       else if (heroi.nivel > 8001 && heroi.nivel < 9000){
-        heroi.liga = "Ascendente"
+       else if (heroi.nivel >= 8001 && heroi.nivel <= 9000){
+         heroi.liga = "Ascendente"
       }
-  
-       else if (heroi.nivel > 9001 && 10000){
-        heroi.liga = "Imortal"
+       else if (heroi.nivel >= 9001 && heroi.nivel <= 10000){
+         heroi.liga = "Imortal"
       } 
-      if (heroi.nivel > 10001){
-        heroi.liga = "Radiante"
+       else if (heroi.nivel >= 10001){
+         heroi.liga = "Radiante"
       }
   
   console.log("Nome do Herói:", heroi.nome)
