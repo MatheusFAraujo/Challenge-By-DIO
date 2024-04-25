18789d3eaade5bc4ce6a644f227c0f9f567b6e56
Matheus Araujo <167690518+MatheusFAraujo@users.noreply.github.com>
2024-04-25 18:28:21 +0530

    Add files via upload

---
+++ ClassificadorHeroi.js
@@ -0,0 +1,44 @@
+const heroi = {
+    nome: "Scannor",
+    sexo: "M",
+    nivel: 9500, 
+    pontosDeVida: 15000,
+  }
+  
+  // Verificando o nível do herói e atribuindo sua liga
+      if (heroi.nivel < 1000) {
+    heroi.liga = "Ferro"
+  } 
+  
+      else if (heroi.nivel > 1000 && heroi.nivel < 2000) {
+    heroi.liga = "Bronze"
+    } 
+  
+      else if (heroi.nivel > 2000 && heroi.nivel < 5000){
+      heroi.liga = "Prata"
+      }
+  
+      else if (heroi.nivel > 5001 && heroi.nivel < 7000){
+        heroi.liga = "Ouro"
+      }
+  
+       else if (heroi.nivel > 7001 && heroi.nivel < 8000) {
+        heroi.liga = "Platina"
+      }
+  
+       else if (heroi.nivel > 8001 && heroi.nivel < 9000){
+        heroi.liga = "Ascendente"
+      }
+  
+       else if (heroi.nivel > 9001 && 10000){
+        heroi.liga = "Imortal"
+      } 
+      if (heroi.nivel > 10001){
+        heroi.liga = "Radiante"
+      }
+  
+  console.log("Nome do Herói:", heroi.nome)
+  console.log("Sexo:", heroi.sexo)
+  console.log("Nível:", heroi.nivel)
+  console.log("Pontos de Vida:", heroi.pontosDeVida)
+  console.log("Liga:", heroi.liga)
